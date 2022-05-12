bdcb5fad31eacc61e72bc9c3f7ccbeda03d78249
Vinicius Salvador Ribeiro <viniciusribeiro113@gmail.com>
2022-05-11 21:00:46 -0300

    Create UserLoginDto and UserController

---
+++ src/main/java/com/springcourse/domain/resource/UserResource.java
@@ -0,0 +1,55 @@
+package com.springcourse.domain.resource;
+
+import com.springcourse.domain.entity.User;
+import com.springcourse.domain.resource.dto.UserLoginDto;
+import com.springcourse.domain.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/users")
+public class UserResource {
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user) {
+        User createdUser = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        user.setId(id);
+        User updatedUser = userService.update(user);
+        return ResponseEntity.ok().body(updatedUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getBy(@PathVariable Long id){
+        User user = userService.getById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll(){
+        return ResponseEntity.ok().body(userService.findAll());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody UserLoginDto userLoginDto){
+        User loggedUser = userService.login(userLoginDto.getEmail(), userLoginDto.getPassword());
+        if (loggedUser != null){
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(loggedUser);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+
+    }
+
+
+
+}
---
+++ src/main/java/com/springcourse/domain/resource/dto/UserLoginDto.java
@@ -0,0 +1,17 @@
+package com.springcourse.domain.resource.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginDto {
+
+    private String email;
+
+    private String password;
+}
